3d36c88333cd4484549cc7f6391db4d7737f1134
lebasquelyza <lebasquelyza66@gmail.com>
2025-09-18 20:17:27 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,1 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Marque @upstash/* comme externes côté server pour éviter la résolution pendant le build
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@upstash/redis': 'commonjs @upstash/redis',
+        '@upstash/ratelimit': 'commonjs @upstash/ratelimit',
+      });
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
 
