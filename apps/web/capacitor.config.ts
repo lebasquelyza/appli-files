d81f021855299b2bbfb792b7b292e7e18e081d4c
lebasquelyza <lebasquelyza66@gmail.com>
2025-11-12 07:01:30 +0100

    Create capacitor.config.ts

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.filescoaching.app',
+  appName: 'Files Coaching',
+  webDir: 'out'
+};
+
+export default config;
