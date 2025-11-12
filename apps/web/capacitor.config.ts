772b091b0c46c53d674cc4dd5a60d465fc3a1434
lebasquelyza <lebasquelyza66@gmail.com>
2025-11-12 07:16:31 +0100

    Update capacitor.config.ts

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -1,9 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.filescoaching.app',
   appName: 'Files Coaching',
-  webDir: 'out'
+  webDir: 'public', // peu importe, ignoré si server.url est défini
+  server: {
+    url: 'https://appli.files-coaching.com', // ← remplace par ton URL
+    cleartext: false
+  }
 };
-
 export default config;
